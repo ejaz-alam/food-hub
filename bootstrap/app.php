c1279a6d4b85c31363d2953cb9803dac41c7bc95
Ejaz <ejaz.alam@deploy.co.uk>
2025-04-18 15:02:02 +0100

    chore: add function comment for description

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+    // register our custom role middleware
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class
